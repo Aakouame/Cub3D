3d93a9b42c9190c3bef7373066df87fe153c054a
Younes Askour <pausedarkf@gmail.com>
2022-12-15 15:22:50 +0100

    working on norminette

--- srcs/free.c
+++ srcs/free.c
@@ -6,38 +6,38 @@
 /*   By: akouame <akouame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 10:55:48 by akouame           #+#    #+#             */
-/*   Updated: 2022/11/30 11:54:02 by akouame          ###   ########.fr       */
+/*   Updated: 2022/12/15 15:22:13 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub.h"
 
-void free_data(t_data *data)
+void	free_data(t_data *data)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(data->all_split[i])
+	while (data->all_split[i])
 		free(data->all_split[i++]);
 	i = 0;
-	while(data->all_splited[i])
+	while (data->all_splited[i])
 		free(data->all_splited[i++]);
 	free(data->all);
 }
 
-void    free_all(char **str)
+void	free_all(char	**str)
 {
-    int    i;
-    int    l;
+	int	i;
+	int	l;
 
-    l = length_tab(str);
-    i = 0;
-    if (!str)
-        return ;
-    while (i < l)
-    {
-        free(str[i]);
-        i++;
-    }
-    free(str);
+	l = length_tab(str);
+	i = 0;
+	if (!str)
+		return ;
+	while (i < l)
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
 }
